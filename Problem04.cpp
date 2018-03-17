9645c2b59c4c3e4819eaad67142ac3965d4f26a1
ctrlaltf2 <23644849+ctrlaltf2@users.noreply.github.com>
2018-03-17 15:11:35 -0400

    Add problem 4

---
+++ Problem04.cpp
@@ -0,0 +1,18 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <string>
+
+int main() {
+    int max{0};
+    for (int a = 999; a > 99; --a) {
+        for (int b = a; b > 99; --b) {
+            std::string product = std::to_string(a * b),
+                        reversed_product = product;
+            std::reverse(reversed_product.begin(), reversed_product.end());
+
+            if (reversed_product == product) max = std::max(max, a * b);
+        }
+    }
+    std::cout << max << '\n';
+}
