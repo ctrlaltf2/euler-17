5d26129616c8cc491b9e391e789657b7eec41bbf
ctrlaltf2 <23644849+ctrlaltf2@users.noreply.github.com>
2018-03-17 10:03:57 -0400

    Removed specializations

--- lib/LazyArray.hpp
+++ lib/LazyArray.hpp
@@ -1,63 +1,87 @@
 #pragma once
 
-#include <cstddef>          // std::ptrdiff_t, std::size_t
-#include <functional>       // std::function
-#include <initializer_list> // std::initializer_list
-#include <iostream>         // std::cout, std::endl
-#include <iterator>         // std::random_access_iterator_tag
-#include <limits>           // std::numeric_limits::max()
-#include <stdexcept>        // std::runtime_error
-#include <type_traits>      // std::is_arithmetic
-#include <utility>          // std::move
-#include <vector>           // std::vector
+#include <cstddef>           // std::ptrdiff_t, std::size_t
+#include <functional>        // std::function
+#include <initializer_list>  // std::initializer_list
+#include <iostream>          // std::cout, std::endl
+#include <iterator>          // std::random_access_iterator_tag
+#include <limits>            // std::numeric_limits::max()
+#include <stdexcept>         // std::runtime_error
+#include <type_traits>       // std::is_arithmetic
+#include <utility>           // std::move
+#include <vector>            // std::vector
 
 /*
- * This 'container' was created to aid with Project Euler problems. The author saw that the use of a
- * lazy evaluated array would prove very useful for some problems.
- * Implements an 'array' that is similar to Haskell's lazy array implementation where just a range
- * or equation is stored for the array, and numbers are generated on the fly when accessed.
- * Currently only polynomials to the nth degree are supported, but this should cover all reasonable
+ * This 'container' was created to aid with Project Euler problems. The author
+ * saw that the use of a lazy evaluated array would prove very useful for some
+ * problems. Implements an 'array' that is similar to Haskell's lazy array
+ * implementation where just a range or equation is stored for the array, and
+ * numbers are generated on the fly when accessed. Currently only polynomials to
+ * the nth degree are supported, but this should cover all reasonable
  * applications.
  * Support as an STL compatible container is planned
  */
 
-template<class T>
+template <class T>
 class LazyArray {
-    typedef std::size_t     size_type;
-    typedef std::ptrdiff_t  difference_type;
-std::function<T(size_type)> m_func;
+    typedef std::size_t size_type;
+    typedef std::ptrdiff_t difference_type;
+    std::function<T(T)> m_func;
     /*
-     * Even though this container is theoretically infinite in size, there does need to be a max value
-     * to stop at for iterators. If this isn't explicitly set through .setEnd(T), things that use .end() 
-     * for traversing the container will keep going for a very, very long time (with some overflows 
+     * Even though this container is theoretically infinite in size, there does
+     * need to be a max value to stop at for iterators. If this isn't explicitly
+     * set through .setEnd(T), things that use .end() for traversing the
+     * container will keep going for a very, very long time (with some overflows
      * thrown in the mix).
      */
     T m_end{std::numeric_limits<T>::max()};
     // By default, this container will begin at zero and count up by 1 to m_end
     T m_begin{0};
-public:
+
+    T m_increment{1};
+
+   public:
     LazyArray() {
-        static_assert(std::is_arithmetic<T>::value, "Arithmetic value required for this container");
+        static_assert(std::is_arithmetic<T>::value,
+                      "Arithmetic value required for this container");
     }
 
     /*
-     * Constructor from a function that returns type T and accepts size_type as a parameter.
+     * Constructor from a function that returns type T and accepts T as
+     * a parameter.
      */
-    LazyArray(std::function<T(size_type)> func) : LazyArray() {
-        m_func = func;
-    }
+    LazyArray(std::function<T(T)> func) { m_func = func; }
 
-    LazyArray(const LazyArray& lz) { m_func = lz.m_func; }
+    /*
+     * Copy constructor
+     */
+    LazyArray(const LazyArray& lz) : m_func{lz.m_func} {}
 
-    LazyArray(LazyArray&& lz) noexcept { m_func = std::move(lz.m_func); }
+    /*
+     * Move constructor
+     */
+    LazyArray(LazyArray&& lz) noexcept : m_func{std::move(lz.m_func)} {}
 
-    // Assignment Operators
-    LazyArray& operator= (LazyArray& other) {
+    /*
+     * Copy assignment
+     */
+    LazyArray& operator=(const LazyArray& other) {
         m_func = other.m_func;
         return *this;
     }
 
-    LazyArray& operator= (std::function<T(size_type)>&& func) {
+    /*
+     * Move assignment
+     */
+    LazyArray& operator=(LazyArray&& other) {
+        m_func = std::move(other.m_func);
+        return *this;
+    }
+
+    /*
+     * Function assignment
+     */
+    LazyArray& operator=(std::function<T(T)>&& func) {
         m_func = func;
         return *this;
     }
@@ -65,25 +89,24 @@ public:
     // Access functions
 
     // No bounds checking
-    T operator[] (const size_type I) {
-        return this->valueAt(I);
-    }
+    T operator[](const T I) const { return this->valueAt(I); }
 
-    T at (const size_type I) {
-        if((I >= m_end) || (I < m_begin))
-            throw std::out_of_range("");
+    // Bounds checking
+    T at(const T I) const {
+        if ((I >= m_end) || (I < m_begin)) throw std::out_of_range("");
 
         return this->valueAt(I);
     }
 
-    T valueAt(const size_type I) {
-        return m_func(I);
-    }
-
+    T valueAt(const T I) const { return m_func(I); }
 
     size_type size() const noexcept { return m_end - m_begin; }
 
-    void swap(LazyArray& other) { std::swap(this->m_func, other.m_func); }
+    void swap(LazyArray<T>& other) {
+        std::swap(this->m_func, other.m_func);
+        std::swap(this->m_begin, other.m_begin);
+        std::swap(this->m_end, other.m_end);
+    }
 
     T getEnd() const noexcept { return m_end; }
 
@@ -91,33 +114,26 @@ public:
     void setEnd() noexcept { m_end = std::numeric_limits<T>::max(); }
     void setEnd(T num) noexcept { m_end = num; }
     void setBegin(T num) noexcept { m_begin = num; }
+    void setIncrement(T num) noexcept { m_increment = num; };
 
     std::vector<T> toVector() {
         std::vector<T> V;
-        for(const auto i : *this)
-            V.push_back(i);
+        for (const auto i : *this) V.push_back(i);
 
         return V;
     }
 
-    class iterator {
-        typedef std::random_access_iterator_tag iterator_category;
-        typedef T               value_type;
-        typedef T*              pointer;
-        typedef T&              reference;
-        typedef std::size_t     size_type;
-        typedef std::ptrdiff_t  difference_type;
-
-        size_type m_position;
+    class iterator : public std::iterator<std::random_access_iterator_tag, T> {
+        T m_position;
         LazyArray* m_parent{nullptr};
 
         friend class LazyArray;
-    public:
 
+       public:
         // http://www.cplusplus.com/reference/iterator/RandomAccessIterator/
-        // Is default-constructible, copy-constructible, copy-assignable and destructible
-        // default constructible
-        iterator() : m_position{0} { }
+        // Is default-constructible, copy-constructible, copy-assignable and
+        // destructible default constructible
+        iterator() : m_position{0} {}
 
         // Copy constructible
         iterator(const iterator& itr) {
@@ -126,44 +142,47 @@ public:
         }
 
         // Copy assignable
-        iterator& operator= (const iterator& itr) {
+        iterator& operator=(const iterator& itr) {
             m_position = itr.m_position;
             m_parent = itr.m_parent;
             return *this;
         }
 
-        // Can be compared for equivalence using the equality/inequality operators
-        // equality
-        bool operator== (const iterator& rhs) {
-            return ((m_position == rhs.m_position) && (m_parent == rhs.m_parent));
+        // Can be compared for equivalence using the equality/inequality
+        // operators equality
+        bool operator==(const iterator& rhs) {
+            return ((m_position == rhs.m_position) &&
+                    (m_parent == rhs.m_parent));
         }
 
         // inequality
-        bool operator!= (const iterator& rhs) { return !(*this == rhs); }
+        bool operator!=(const iterator& rhs) { return !(*this == rhs); }
 
         // Can be dereferenced as an rvalue (if in a dereferenceable state).
-        T operator* () { return m_parent->at(m_position); }
+        T operator*() { return m_parent->at(m_position); }
 
-        // ???: operator -> is required so that iterator->member is valid but is this ever used?
+        // ???: operator -> is required so that iterator->member is valid
+        // but is this ever used?
 
         // Can be incremented (if in a dereferenceable state).
         // prefix increment
         iterator& operator++() {
-            ++m_position;
+            //++m_position;
+            m_position += m_parent->m_increment;
             return *this;
         }
 
         // postfix increment
-        iterator operator++ (int) {
+        iterator operator++(int) {
             iterator t = *this;
             ++*this;
             return t;
         }
 
-        // Can be decremented (if a dereferenceable iterator value precedes it).
-        // prefix decrement
+        // Can be decremented (if a dereferenceable iterator value precedes
+        // it). prefix decrement
         iterator& operator--() {
-            --m_position;
+            m_position -= m_parent->m_increment;
             return *this;
         }
 
@@ -174,41 +193,50 @@ public:
             return t;
         }
 
-        // Supports the arithmetic operators + and - between an iterator and an integer value, or subtracting an iterator from another.
-        // add integer
-        iterator operator+ (const difference_type n) {
+        // Supports the arithmetic operators + and - between an iterator and
+        // an integer value, or subtracting an iterator from another. add
+        // integer
+        iterator operator+(const difference_type n) {
             m_position += n;
             return *this;
         }
 
         // subtract integer
-        iterator operator- (const difference_type n) {
+        iterator operator-(const difference_type n) {
             m_position -= n;
             return *this;
         }
 
         // subtract iterators
-        difference_type operator- (const iterator& other) { return m_position - other.m_position; }
+        difference_type operator-(const iterator& other) {
+            return m_position - other.m_position;
+        }
 
-        // Can be compared with inequality relational operators (<, >, <= and >=).
-        bool operator< (const iterator& rhs) { return m_position < rhs.m_position; }
-        bool operator> (const iterator& rhs) { return rhs.m_position < m_position; }
+        // Can be compared with inequality relational operators (<, >, <=
+        // and
+        // >=).
+        bool operator<(const iterator& rhs) {
+            return m_position < rhs.m_position;
+        }
+        bool operator>(const iterator& rhs) {
+            return rhs.m_position < m_position;
+        }
         bool operator<=(const iterator& rhs) { return !(*this > rhs); }
         bool operator>=(const iterator& rhs) { return !(*this < rhs); }
 
         // Supports compound assignment operations += and -=
-        iterator& operator+= (const size_type n) {
+        iterator& operator+=(const size_type n) {
             m_position += n;
             return *this;
         }
 
-        iterator& operator-= (const size_type n) {
+        iterator& operator-=(const size_type n) {
             m_position -= n;
             return *this;
         }
 
         // Supports the offset dereference operator ([])
-        T operator[] (const difference_type t) { return *((*this) + t); }
+        T operator[](const difference_type t) { return *((*this) + t); }
 
         // Lvalues are swappable.
         void swap(iterator& lhs, iterator& rhs) {
@@ -216,8 +244,7 @@ public:
             std::swap(lhs.m_parent, rhs.m_parent);
         }
 
-
-    }; // class iterator
+    };  // class iterator
 
     iterator begin() {
         LazyArray<T>::iterator itr;
@@ -233,172 +260,4 @@ public:
         return itr;
     }
 
-}; // class LazyArray
-
-// Specialization for iterator_traits because somehow typedefs inside the iterator just isn't enough
-
-namespace std {
-    // generated using the python file included in lib/
-    template<>
-    struct iterator_traits<LazyArray<bool>::iterator> {
-        typedef std::random_access_iterator_tag iterator_category;
-        typedef bool               value_type;
-        typedef bool*              pointer;
-        typedef bool&              reference;
-        typedef std::size_t     size_type;
-        typedef std::ptrdiff_t  difference_type;
-    };
-    template<>
-    struct iterator_traits<LazyArray<char>::iterator> {
-        typedef std::random_access_iterator_tag iterator_category;
-        typedef char               value_type;
-        typedef char*              pointer;
-        typedef char&              reference;
-        typedef std::size_t     size_type;
-        typedef std::ptrdiff_t  difference_type;
-    };
-    template<>
-    struct iterator_traits<LazyArray<char16_t>::iterator> {
-        typedef std::random_access_iterator_tag iterator_category;
-        typedef char16_t               value_type;
-        typedef char16_t*              pointer;
-        typedef char16_t&              reference;
-        typedef std::size_t     size_type;
-        typedef std::ptrdiff_t  difference_type;
-    };
-    template<>
-    struct iterator_traits<LazyArray<char32_t>::iterator> {
-        typedef std::random_access_iterator_tag iterator_category;
-        typedef char32_t               value_type;
-        typedef char32_t*              pointer;
-        typedef char32_t&              reference;
-        typedef std::size_t     size_type;
-        typedef std::ptrdiff_t  difference_type;
-    };
-    template<>
-    struct iterator_traits<LazyArray<wchar_t>::iterator> {
-        typedef std::random_access_iterator_tag iterator_category;
-        typedef wchar_t               value_type;
-        typedef wchar_t*              pointer;
-        typedef wchar_t&              reference;
-        typedef std::size_t     size_type;
-        typedef std::ptrdiff_t  difference_type;
-    };
-    template<>
-    struct iterator_traits<LazyArray<signed char>::iterator> {
-        typedef std::random_access_iterator_tag iterator_category;
-        typedef signed char               value_type;
-        typedef signed char*              pointer;
-        typedef signed char&              reference;
-        typedef std::size_t     size_type;
-        typedef std::ptrdiff_t  difference_type;
-    };
-    template<>
-    struct iterator_traits<LazyArray<short int>::iterator> {
-        typedef std::random_access_iterator_tag iterator_category;
-        typedef short int               value_type;
-        typedef short int*              pointer;
-        typedef short int&              reference;
-        typedef std::size_t     size_type;
-        typedef std::ptrdiff_t  difference_type;
-    };
-    template<>
-    struct iterator_traits<LazyArray<int>::iterator> {
-        typedef std::random_access_iterator_tag iterator_category;
-        typedef int               value_type;
-        typedef int*              pointer;
-        typedef int&              reference;
-        typedef std::size_t     size_type;
-        typedef std::ptrdiff_t  difference_type;
-    };
-    template<>
-    struct iterator_traits<LazyArray<long int>::iterator> {
-        typedef std::random_access_iterator_tag iterator_category;
-        typedef long int               value_type;
-        typedef long int*              pointer;
-        typedef long int&              reference;
-        typedef std::size_t     size_type;
-        typedef std::ptrdiff_t  difference_type;
-    };
-    template<>
-    struct iterator_traits<LazyArray<long long int>::iterator> {
-        typedef std::random_access_iterator_tag iterator_category;
-        typedef long long int               value_type;
-        typedef long long int*              pointer;
-        typedef long long int&              reference;
-        typedef std::size_t     size_type;
-        typedef std::ptrdiff_t  difference_type;
-    };
-    template<>
-    struct iterator_traits<LazyArray<unsigned char>::iterator> {
-        typedef std::random_access_iterator_tag iterator_category;
-        typedef unsigned char               value_type;
-        typedef unsigned char*              pointer;
-        typedef unsigned char&              reference;
-        typedef std::size_t     size_type;
-        typedef std::ptrdiff_t  difference_type;
-    };
-    template<>
-    struct iterator_traits<LazyArray<unsigned short int>::iterator> {
-        typedef std::random_access_iterator_tag iterator_category;
-        typedef unsigned short int               value_type;
-        typedef unsigned short int*              pointer;
-        typedef unsigned short int&              reference;
-        typedef std::size_t     size_type;
-        typedef std::ptrdiff_t  difference_type;
-    };
-    template<>
-    struct iterator_traits<LazyArray<unsigned int>::iterator> {
-        typedef std::random_access_iterator_tag iterator_category;
-        typedef unsigned int               value_type;
-        typedef unsigned int*              pointer;
-        typedef unsigned int&              reference;
-        typedef std::size_t     size_type;
-        typedef std::ptrdiff_t  difference_type;
-    };
-    template<>
-    struct iterator_traits<LazyArray<unsigned long int>::iterator> {
-        typedef std::random_access_iterator_tag iterator_category;
-        typedef unsigned long int               value_type;
-        typedef unsigned long int*              pointer;
-        typedef unsigned long int&              reference;
-        typedef std::size_t     size_type;
-        typedef std::ptrdiff_t  difference_type;
-    };
-    template<>
-    struct iterator_traits<LazyArray<unsigned long long int>::iterator> {
-        typedef std::random_access_iterator_tag iterator_category;
-        typedef unsigned long long int               value_type;
-        typedef unsigned long long int*              pointer;
-        typedef unsigned long long int&              reference;
-        typedef std::size_t     size_type;
-        typedef std::ptrdiff_t  difference_type;
-    };
-    template<>
-    struct iterator_traits<LazyArray<float>::iterator> {
-        typedef std::random_access_iterator_tag iterator_category;
-        typedef float               value_type;
-        typedef float*              pointer;
-        typedef float&              reference;
-        typedef std::size_t     size_type;
-        typedef std::ptrdiff_t  difference_type;
-    };
-    template<>
-    struct iterator_traits<LazyArray<double>::iterator> {
-        typedef std::random_access_iterator_tag iterator_category;
-        typedef double               value_type;
-        typedef double*              pointer;
-        typedef double&              reference;
-        typedef std::size_t     size_type;
-        typedef std::ptrdiff_t  difference_type;
-    };
-    template<>
-    struct iterator_traits<LazyArray<long double>::iterator> {
-        typedef std::random_access_iterator_tag iterator_category;
-        typedef long double               value_type;
-        typedef long double*              pointer;
-        typedef long double&              reference;
-        typedef std::size_t     size_type;
-        typedef std::ptrdiff_t  difference_type;
-    };
-}
+};  // class LazyArray
--- lib/LazyArrayTest.cpp
+++ lib/LazyArrayTest.cpp
@@ -1,32 +1,42 @@
-#include "LazyArray.hpp"
 #include <algorithm>
 #include <numeric>
+#include "LazyArray.hpp"
 
 int main() {
-    // LazyArray<std::string> LZ; // Should fail to compile and raise a static assert error
-    LazyArray<long> LZ( [](const std::size_t i)->long { return i * i; } );
+    // LazyArray<std::string> LZ; // Should fail to compile and raise a static
+    // assert error
+    LazyArray<long> LZ([](const std::size_t i) -> long { return i * i; });
     std::cout << "Function test: Should be '4 16'" << std::endl;
     std::cout << LZ[2] << ' ' << LZ[4] << std::endl;
-    std::cout << "Max test: Should be the max value for whatever type is used for above" << std::endl;
-    std::cout << LZ.getEnd() << std::endl; // Should print the std::numeric_limits::max() of the LazyArray type
+    std::cout << "Max test: Should be the max value for whatever type is used "
+                 "for above"
+              << std::endl;
+    std::cout << LZ.getEnd() << std::endl;  // Should print the
+                                            // std::numeric_limits::max() of the
+                                            // LazyArray type
 
     LazyArray<long> LZCopy(LZ);
     std::cout << "Copy constructor test: Should print '4 16'" << std::endl;
-    std::cout << LZCopy[2] << ' ' << LZCopy[4] << std::endl; 
+    std::cout << LZCopy[2] << ' ' << LZCopy[4] << std::endl;
 
-    LazyArray<long> LZSwapTest( [](const std::size_t i)->long { return i + 1; });;
+    LazyArray<long> LZSwapTest(
+        [](const std::size_t i) -> long { return i + 1; });
+    ;
     LZSwapTest.swap(LZCopy);
     std::cout << "Swap test: Should print '4 16'" << std::endl;
     std::cout << LZSwapTest[2] << ' ' << LZSwapTest[4] << std::endl;
     std::cout << "Swap test: Should print '1 2 3'" << std::endl;
     std::cout << LZCopy[0] << ' ' << LZCopy[1] << ' ' << LZCopy[2] << std::endl;
 
-    LZCopy = [](const std::size_t i)->long { return i + 2; };
-    std::cout << "Operator= initializer list test: Should print '2 3'" << std::endl;
+    LZCopy = [](const std::size_t i) -> long { return i + 2; };
+    std::cout << "Operator= initializer list test: Should print '2 3'"
+              << std::endl;
     std::cout << LZCopy[0] << ' ' << LZCopy[1] << ' ' << std::endl;
 
-    // LZCopy.end(); // Should throw a compile-time error saying that the container is infinite in size
-    LazyArray<long> AccessTest( [](const int x)->long { return 3*x+7; });;
+    // LZCopy.end(); // Should throw a compile-time error saying that the
+    // container is infinite in size
+    LazyArray<long> AccessTest([](const int x) -> long { return 3 * x + 7; });
+    ;
     std::cout << AccessTest.at(2) << std::endl;
 
     std::cout << AccessTest.size() << std::endl;
@@ -34,10 +44,10 @@ int main() {
     int i = 0;
     AccessTest.setEnd(20);
     std::cout << "Catch out of bounds test: " << std::endl;
-    while(true) {
+    while (true) {
         try {
             std::cout << AccessTest.at(i) << ' ';
-        } catch(std::out_of_range&) {
+        } catch (std::out_of_range&) {
             std::cout << std::endl;
             break;
         }
@@ -45,14 +55,14 @@ int main() {
     }
 
     std::cout << "Index test: " << std::endl;
-    for(int i = 0; i < AccessTest.size(); ++i) {
+    for (int i = 0; i < AccessTest.size(); ++i) {
         std::cout << AccessTest[i] << ' ';
     }
     std::cout << std::endl;
 
     std::cout << "for(:) test: " << std::endl;
     AccessTest.setBegin(3);
-    for(const auto i : AccessTest) {
+    for (const auto i : AccessTest) {
         std::cout << i << ' ';
     }
     std::cout << std::endl;
@@ -67,26 +77,51 @@ int main() {
     std::cout << "end > begin: " << (itrend > itr) << std::endl;
     std::cout << "begin > end: " << (itr > itrend) << std::endl;
 
-    // Algorithms library test (Some will not work because the elements don't actually exist
-    std::cout << "std::all_of testing if all numbers are greater than zero: "; 
-    std::cout << std::all_of(AccessTest.begin(), AccessTest.end(), [](const long l)->bool { return l > 0; }) << std::endl; 
+    // Algorithms library test (Some will not work because the elements don't
+    // actually exist
+    std::cout << "std::all_of testing if all numbers are greater than zero: ";
+    std::cout << std::all_of(AccessTest.begin(), AccessTest.end(),
+                             [](const long l) -> bool { return l > 0; })
+              << std::endl;
 
     std::cout << "std::any_of testing if any numbers are divisible by three: ";
-    std::cout << std::any_of(AccessTest.begin(), AccessTest.end(), [](const long l)->bool { return l % 3 == 0; }) << std::endl;;
+    std::cout << std::any_of(AccessTest.begin(), AccessTest.end(),
+                             [](const long l) -> bool { return l % 3 == 0; })
+              << std::endl;
+    ;
 
-    std::cout << "std::none_of testing if none of the numbers are negative" << std::endl;
-    std::cout << std::none_of(AccessTest.begin(), AccessTest.end(), [](const long l)->bool { return l < 0; }) << std::endl;
+    std::cout << "std::none_of testing if none of the numbers are negative"
+              << std::endl;
+    std::cout << std::none_of(AccessTest.begin(), AccessTest.end(),
+                              [](const long l) -> bool { return l < 0; })
+              << std::endl;
 
-    std::cout << "std::for_each to print the squares of what's inside AccessTest: " << std::endl;
-    std::for_each(AccessTest.begin(), AccessTest.end(), [](const auto num)->void { std::cout << num * num << std::endl; });
+    std::cout
+        << "std::for_each to print the squares of what's inside AccessTest: "
+        << std::endl;
+    std::for_each(
+        AccessTest.begin(), AccessTest.end(),
+        [](const auto num) -> void { std::cout << num * num << std::endl; });
 
     std::cout << "std::count_if to see how many values are even" << std::endl;
-    std::cout << std::count_if(AccessTest.begin(), AccessTest.end(), [](const auto num)->bool { return num % 2; }) << std::endl;
+    std::cout << std::count_if(AccessTest.begin(), AccessTest.end(),
+                               [](const auto num) -> bool { return num % 2; })
+              << std::endl;
 
-    std::cout << "std::mismatch would require conversion to a container with values to work" << std::endl;
+    std::cout << "std::mismatch would require conversion to a container with "
+                 "values to work"
+              << std::endl;
     std::vector<long> V = AccessTest.toVector();
 
-    for(const auto& i : V) std::cout << i << ' ';
+    for (const auto& i : V) std::cout << i << ' ';
     std::cout << std::endl;
+
+    {
+        LazyArray<float> lz([](const float x) { return x; });
+        lz.setEnd(9.5);
+        lz.setIncrement(0.5);
+        for (const float i : lz) std::cout << i << ' ';
+        std::cout << '\n';
+    }
     return 0;
 }
